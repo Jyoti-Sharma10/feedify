790d708489ac3970bbb03e716d417f17df49305f
Jyoti <js10092000@gmail.com>
2023-07-13 00:24:48 +0530

    backend added

---
+++ backend/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const mongoURI = 'mongodb+srv://feedify:feedify123@cluster0.pquszpk.mongodb.net/feedify?retryWrites=true&w=majority';
+
+const mongoDB = async () => {
+  try {
+    await mongoose.connect(mongoURI, { useNewUrlParser: true });
+    console.log('Connected to MongoDB');
+    const fetched_data = mongoose.connection.db.collection('food_items');
+    const data = await fetched_data.find({}).toArray(); 
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+};
+
+module.exports = mongoDB;
