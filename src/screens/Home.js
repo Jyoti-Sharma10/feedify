01c0fd8c43f18fab0c6254ad3e2f22e140adb937
Jyoti <js10092000@gmail.com>
2023-07-16 18:06:18 +0530

    search functionality added

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,15 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
-import Poster from '../components/Poster'
-import Slider from '../components/Slider';
+import Poster from '../components/Poster';
 import Carousel from 'react-bootstrap/Carousel';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
+const carouselImageStyle = {
+  objectFit: "cover",
+  margin: "auto",
+  maxHeight: "80vh",
+  filter: "brightness(30%)",
+};
+
 export default function Home() {
   const[foodCat, setFoodCat] = useState([]);
   const[foodItem, setFoodItem] = useState([]);
+  const[search, setSearch] = useState('');
 
   const loadData = async () => {
     let response = await fetch('http://localhost:5000/api/foodData',{
@@ -34,7 +41,53 @@ export default function Home() {
   return (
     <div>
     <div><Header/></div>
-    <div><Slider/></div>
+    <div style={{ minWidth: "100vw", position: "relative" }}>
+  <div style={{ width: "60%", margin: "0 auto" }}>
+    <div id="carousel-inner">
+      <Carousel fade>
+        <Carousel.Item>
+          <img
+            style={carouselImageStyle}
+            className="d-block w-100"
+            src="https://source.unsplash.com/random/?Muffins"
+            alt="First slide"
+          />
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img
+            style={carouselImageStyle}
+            className="d-block w-100"
+            src="https://source.unsplash.com/random/?Spaghetti"
+            alt="Second slide"
+          />
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img
+            style={carouselImageStyle}
+            className="d-block w-100"
+            src="https://source.unsplash.com/random/?burger"
+            alt="Third slide"
+          />
+        </Carousel.Item>
+      </Carousel>
+        <div className="d-flex justify-content-center mb-3" style={{ position: "absolute", zIndex: "2", bottom: "0", width: "60%", margin: "0 auto"}}>
+          <Form.Control
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+            value={search}
+            onChange={(e) => {setSearch(e.target.value)}}
+          />
+          <Button variant="outline-success">Search</Button>
+        </div>
+    </div>
+  </div>
+</div>
+
+
       <div className='container'>
         {
           foodCat !== [] ? foodCat.map((data) => {
@@ -44,7 +97,7 @@ export default function Home() {
                 {data.CategoryName} </div>
                 <hr/>
                 {
-                  foodItem !== [] ? foodItem.filter((item) => item.CategoryName === data.CategoryName)
+                  foodItem !== [] ? foodItem.filter((item) => (item.CategoryName === data.CategoryName) && (item.name.toLowerCase().includes(search.toLocaleLowerCase())) )
                   .map( filterItems => {
                     return (
                       <div key={filterItems._id} className='col-12 col-md-6 col-lg-3'>
